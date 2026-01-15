b62db4b9586b000a7454fdd3f96087ebff03d1d5
notYAS <yasserfat7alah@gmail.com>
2026-01-15 03:39:14 +0200

    chore(setup): initialize clean react app and setup tailwind:
    - Initialized react app using vite
    
    - Cleaned the boiler plate code
    
    - Installed tailwindCSS and configured V4 with vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer"
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </>
+  )
+}
+
+export default App
